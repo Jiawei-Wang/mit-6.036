b105cadcade102eecc9d2802c3781426e2c0167b
Jiawei Wang <wangjiawei_med@hotmail.com>
2024-06-18 16:43:24 -0400

    start course

---
+++ Readiness Assessment.py
@@ -0,0 +1,26 @@
+"""
+1.1) Row and column vectors
+
+Two vectors:
+a = [1 2 3]
+b = [4 5 6]
+
+
+To add two equal length vectors, we simply add the corresponding elements in each vector to produce a new same-length vector.
+a + b = [5 7 9]
+a + 10b = [41 52 63]
+
+We multiply each element in the first vector by the corresponding element in the second vector, producing the same-size result vector.
+a * b = [4 10 18]
+
+a row vector and a column vector cannot be added together
+
+
+1.2) Vector dot product
+a ⋅ b = 32
+The dot product is the sum of the element-wise product of the two vectors
+
+
+1.3) 2d arrays and matrices
+
+"""
